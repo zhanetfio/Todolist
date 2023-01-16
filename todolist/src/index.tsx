1b6faa71583963baf17d21efac1fdc529997b149
Zhanet <zhanet_f@mail.ru>
2023-01-16 14:24:14 +0300

    feat and refactor: use ReduxToolkit

--- todolist/src/index.tsx
+++ todolist/src/index.tsx
@@ -5,16 +5,16 @@ import reportWebVitals from './reportWebVitals';
 import App from "./App";
 import {Provider} from "react-redux";
 import {store} from "./state/store";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
             <App/>
-        </BrowserRouter>
+        </HashRouter>
     </Provider>
 );
 
