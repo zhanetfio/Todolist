321a074c2b1ad65034db921d522283ffa2350784
Zhanet <zhanet_f@mail.ru>
2023-01-16 20:14:55 +0300

    fix:gates

--- todolist/src/state/hooks.ts
+++
@@ -1,7 +0,0 @@
-import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
-import {AppDispatch, AppRootStateType} from "./store";
-
-export  const useAppDispatch=()=> useDispatch<AppDispatch>()
-export const useAppSelector: TypedUseSelectorHook<AppRootStateType>=useSelector
-
-/*AppDispatch=useDispatch*/
